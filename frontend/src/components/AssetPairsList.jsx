f85ecca93fe835e66cc65814c04336923ffdaae8
Sergey <mr.nosomebody@gmail.com>
2023-01-08 21:50:22 +0400

    frontend fully refactored; components

---
+++ frontend/src/components/AssetPairsList.jsx
@@ -0,0 +1,29 @@
+import React, {useState, useEffect} from "react";
+import AssetPairItem from "./AssetPairItem";
+
+import axios from "axios";
+
+const AssetPairsList = (props) => {
+    const [assetPairs, setAssetPairs] = useState([])
+
+    useEffect(() => {
+        axios.get('http://localhost:8002/api/asset-pairs/')
+            .then(response => {
+                setAssetPairs(response.data);
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    }, []);
+
+    return (
+        <div className="">
+            {assetPairs.map(assetPair => (
+                <AssetPairItem asset_pair={assetPair} key={assetPair.id}></AssetPairItem>
+            ))}
+        </div>
+    )
+
+}
+
+export default AssetPairsList;
